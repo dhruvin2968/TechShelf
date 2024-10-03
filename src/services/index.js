74ba8a39af5b8a6d8817beadbd06e89a5c93b2ee
Dhruvin <therockstar3007@gmail.com>
2024-10-03 15:53:40 +0530

    The final commit

--- src/services/index.js
+++ src/services/index.js
@@ -1,3 +1,3 @@
 export { login, register, logout } from "./authService";
-export {createOrder, getUser, getUserOrders } from "./dataService";
 export { getProductList, getProduct, getFeaturedList } from "./productService";
+export {createOrder, getUser, getUserOrders } from "./dataService";
